58665c23b0ebe0ae0339d932fa94d7d2b81af26c
Ryosuke <tarou2960@gmail.com>
2024-04-22 21:08:01 +0900

    エラーでたのでやりなおし

--- src/components/hooks/useTodoManagement.ts
+++ src/components/hooks/useTodoManagement.ts
@@ -1,24 +1,38 @@
-// hooks/useTodoManagement.js
+import { Dispatch, SetStateAction, useState } from "react";
 import useCreateTodo from "./useCreateTodo";
 import useUpdateTodos from "./useUpdateTodos";
 import useHandleSubmit from "./useHandleSubmit";
+import { Todo } from "../models/interface";
 
 function useTodoManagement(
-  inputValue,
-  todos,
-  setTodos,
-  handleChange,
-  validateInput
+  inputValue: string,
+  todos: Todo[],
+  setTodos: Dispatch<SetStateAction<Todo[]>>,
+  handleChange: React.ChangeEventHandler<HTMLInputElement>
 ) {
+  const [error, setError] = useState<string>("");
   const { createTodo } = useCreateTodo(inputValue);
   const { updateTodos } = useUpdateTodos(todos, setTodos, handleChange);
-  const { handleSubmit } = useHandleSubmit(
-    validateInput,
-    createTodo,
-    updateTodos
-  );
 
-  return { createTodo, updateTodos, handleSubmit };
+  const validateInput = () => {
+    const isValid = inputValue.trim().length > 0;
+    if (!isValid) {
+      setError("!");
+      return false;
+    }
+    setError("");
+    return true;
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (validateInput()) {
+      const newTodo = createTodo();
+      updateTodos(newTodo);
+    }
+  };
+
+  return { createTodo, updateTodos, handleSubmit, error };
 }
 
 export default useTodoManagement;
