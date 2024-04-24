30dcbdd1300a557545e8ca3d1dc5782d04128a5b
Ryosuke <tarou2960@gmail.com>
2024-04-24 15:29:41 +0900

    レイアウト微調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const Page = () => {
     <main>
       <form onSubmit={handleSubmit} className="mb-5 relative flex justify-between smd:justify-start">
         <input type="text" value={inputValue} onChange={handleChange}
-          placeholder="入力欄 ..." className="border-2 p-2 rounded mr-2 w-9/12 focus:outline-none focus:border-yellow-950"/>
+          placeholder="入力欄 ..." className="border-2 py-2 pl-3 rounded mr-2 w-9/12 focus:outline-none focus:border-yellow-950"/>
         {error && <div className="error">{error}</div>}
         <button type="submit" className="bg-emerald-600 text-white font-bold py-2 px-3.5 rounded ss:px-4">追加</button>
       </form>
