5cb0d59b646691b4c2a9e4126e3936e7023be38b
Ryosuke <tarou2960@gmail.com>
2024-04-26 00:16:29 +0900

    UIの分別化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@
 import React from "react";
 import useTodoManagement from "@/components/client/hooks/useTodoManagement";
 import useScrollFixed from "@/components/client/hooks/useScrollFixed";
-import useDropTodo from "@/components/client/hooks/useDropTodo";
 import { TodoProvider } from "@/components/client/context/TodoContext";
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 import ClearListButton from "@/components/client/ui/ClearListButton.client";
 import UndoListButton from "@/components/client/ui/UndoListButton.client";
+import TodoList from "@/components/client/ui/TodoList";
+import TodoForm from "@/components/client/ui/TodoFrom";
 
 const Page = () => {
   const {
@@ -24,81 +24,29 @@ const Page = () => {
     removeItem,
   } = useTodoManagement();
   const { placeholderStyle, formRef, fixedStyle } = useScrollFixed();
-  const { onDragEnd } = useDropTodo(todos, setTodos);
 
   return (
     <TodoProvider>
       <main>
         <div style={placeholderStyle}></div>
         <div ref={formRef} style={fixedStyle}>
-          <form
-            onSubmit={handleSubmit}
-            className="relative flex justify-between smd:justify-start w-full"
-          >
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleChange}
-              placeholder="入力欄 ..."
-              className="border-2 py-2 pl-3 rounded mr-2 w-9/12 focus:outline-none focus:border-yellow-950"
-            />
-            {error && <div className="error">{error}</div>}
-            <button
-              type="submit"
-              className="bg-emerald-600 text-white font-bold py-2 px-3.5 rounded ss:px-4"
-            >
-              追加
-            </button>
-          </form>
+          <TodoForm
+            inputValue={inputValue}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            error={error}
+          />
         </div>
         {loading ? (
           <div className="text-stone-500">リスト読み込み中...</div>
         ) : (
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="todos-list" direction="vertical">
-              {(provided) => (
-                <ul
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                  className="pb-20 pt-5"
-                >
-                  {todos.map((todo, index) => (
-                    <Draggable
-                      key={todo.id}
-                      draggableId={todo.id.toString()}
-                      index={index}
-                    >
-                      {(provided, snapshot) => (
-                        <li
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          className={`Todolist flex justify-between items-center ${
-                            snapshot.isDragging ? "dragging" : ""
-                          } ${selectedId === todo.id ? "selected" : ""}`}
-                          onClick={() => handleSelect(todo.id)}
-                        >
-                          <span className="listItem">{todo.text}</span>
-                          <div
-                            onClick={(e) => e.stopPropagation()}
-                            className="h-6 w-6 mr-3"
-                          >
-                            <input
-                              type="checkbox"
-                              checked={todo.completed}
-                              onChange={() => toggleTodoComplete(todo.id)}
-                              className="h-7 w-7 accent-green-700"
-                            />
-                          </div>
-                        </li>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </ul>
-              )}
-            </Droppable>
-          </DragDropContext>
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            selectedId={selectedId}
+            handleSelect={handleSelect}
+            toggleTodoComplete={toggleTodoComplete}
+          />
         )}
         <ClearListButton
           todos={todos}
